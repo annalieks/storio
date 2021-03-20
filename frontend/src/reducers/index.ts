56978375040312b7658aef78b0715904d4442f34
Anna Alieksieienko <55431139+annalieks@users.noreply.github.com>
2021-03-20 12:40:43 +0200

    Modification/login form (#13)
    * Connect login on backend and frontend side
    
    * fixup! Refactoring
    
    * Add error toastr
    
    Co-authored-by: Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>

--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as toastr } from 'react-redux-toastr';
 import auth from '@containers/AuthForm/reducer';
 
 export default combineReducers({
   auth,
+  toastr,
 });
